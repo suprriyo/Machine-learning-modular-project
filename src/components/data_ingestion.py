8b17bf177dfa8e143ab4a5599da19b8075f8096c
Supriyo chakraborty <supriyochakraborty954@gmail.com>
2024-07-13 13:04:49 +0530

    data_transormation_done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.logger import logging
 from src.exception import CustomException
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 
 @dataclass
@@ -53,5 +54,7 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data_path,test_data_path=obj.iniitiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_ = data_transformation.inititate_data_transformation(train_data_path,test_data_path)
     
     
--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -24,3 +24,13 @@ RAW_DATA_DIR_KEY = "raw.csv"
 TRAIN_DATA_DIR_KEY = "train.csv"
 TEST_DATA_DIR_KEY = "test.csv"
 
+# Data Transformation related variable
+
+DATA_TRANSFORMATION_ARTIFACT = "data_transformation"
+DATA_PREPROCCED_DIR = "procceor"
+DATA_TRANSFORMTION_PROCESSING_OBJ = "processor.pkl"
+DATA_TRANSFORM_DIR = "transformation"
+TRANSFORM_TRAIN_DIR_KEY = "train.csv"
+TRANSFORM_TEST_DIR_KEY = "test.csv"
+#os.path.join(ROOT_DIR, ARTIFACT_DIR_KEY, DATA_TRANSFORMATION_ARTIFACT,
+       #                                  DATA_TRANSFORM_DIR)
